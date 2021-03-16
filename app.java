cb6217196c175f7470cfb59937aa4cd59c4fd9f2
sudheer307 <44992148+sudheer307@users.noreply.github.com>
2021-03-16 10:05:11 +0530

    Update app.java

--- app.java
+++ app.java
@@ -8,3 +8,13 @@ class Main {
     }
   }
 }
+class Main {
+  public static void main(String[] args) {
+
+    int n = 5;
+    // for loop  
+    for (int i = 1; i <= n; ++i) {
+      System.out.println("Java is fun");
+    }
+  }
+
